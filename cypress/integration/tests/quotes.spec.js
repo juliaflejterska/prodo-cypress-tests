eeec8c0a337da6e02be9d638b0a8fa3f48ac1a45
juliaflejterska <flejterskajulia@gmail.com>
2023-11-29 18:52:35 +0100

    contexts added

--- cypress/integration/tests/quotes.spec.js
+++ cypress/integration/tests/quotes.spec.js
@@ -7,33 +7,35 @@ describe("Quotes - Functionality", () => {
     cy.loginWithValidCredentials();
   });
 
-  it("Initially displays the first quote and changes the quote on button click", () => {
-    cy.wait(2500);
+  context("Displaying quotes", () => {
+    it("Initially displays the first quote and changes the quote on button click", () => {
+      cy.wait(2500);
 
-    quotesPage
-      .getQuote()
-      .should("be.visible")
-      .invoke("text")
-      .should("match", /"(.*)"/);
-    quotesPage
-      .getQuoteAuthor()
-      .should("be.visible")
-      .invoke("text")
-      .should("not.be.empty");
+      quotesPage
+        .getQuote()
+        .should("be.visible")
+        .invoke("text")
+        .should("match", /"(.*)"/);
+      quotesPage
+        .getQuoteAuthor()
+        .should("be.visible")
+        .invoke("text")
+        .should("not.be.empty");
 
-    quotesPage
-      .getQuote()
-      .invoke("text")
-      .then((quote) => {
-        quotesPage.changeQuote();
-        cy.wait(2500);
-        quotesPage.getQuote().invoke("text").should("not.equal", quote);
-      });
-  });
+      quotesPage
+        .getQuote()
+        .invoke("text")
+        .then((quote) => {
+          quotesPage.changeQuote();
+          cy.wait(2500);
+          quotesPage.getQuote().invoke("text").should("not.equal", quote);
+        });
+    });
 
-  it("Dynamically loads quotes from an external quote API", () => {
-    quotesPage.loadQuotesFromExternalAPI();
-    quotesPage.changeQuote();
-    cy.wait("@quoteAPI").its("response.statusCode").should("eq", 200);
+    it("Dynamically loads quotes from an external quote API", () => {
+      quotesPage.loadQuotesFromExternalAPI();
+      quotesPage.changeQuote();
+      cy.wait("@quoteAPI").its("response.statusCode").should("eq", 200);
+    });
   });
 });
